49f53c633b30cc4f0d57230143809d1396ef5120
alex <alex@rocketguys.com>
2018-12-07 21:17:29 +0300

    init

---
+++ main.py
@@ -0,0 +1,36 @@
+from telegram.ext import Updater, CommandHandler
+
+REQUEST_KWARGS = {
+    'proxy_url': 'socks5://37.139.30.202:5656',
+    # Optional, if you need authentication:
+    'urllib3_proxy_kwargs': {
+        'username': 'proxyuser',
+        'password': 'n9jXHGHE00Arj42eqn',
+      }
+}
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+def start(bot, update):
+    update.message.reply_text(
+        'Hi, hi: {}'.format(update.message.from_user.first_name))
+
+print('wtf')
+
+updater = Updater('778814536:AAGZp7325xBl2fb73PgAI7bQTYfV9Ao44sk',
+                  request_kwargs=REQUEST_KWARGS)
+
+print('setup')
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('start', start))
+
+print('get_me')
+print(updater.bot.get_me())
+
+print('start_polling')
+
+updater.start_polling()
+updater.idle()
