fd925ccc50f30f900be797df3c8c2141ca46cfb4
alex <alex@rocketguys.com>
2018-12-07 22:56:46 +0300

    and...

--- index.py
+++ index.py
@@ -13,29 +13,29 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write(message.encode())
         return
 
-# REQUEST_KWARGS = {
-#     'proxy_url': 'socks5://37.139.30.202:5656',
-#     # Optional, if you need authentication:
-#     'urllib3_proxy_kwargs': {
-#         'username': 'proxyuser',
-#         'password': 'n9jXHGHE00Arj42eqn',
-#       }
-# }
+REQUEST_KWARGS = {
+    'proxy_url': 'socks5://37.139.30.202:5656',
+    # Optional, if you need authentication:
+    'urllib3_proxy_kwargs': {
+        'username': 'proxyuser',
+        'password': 'n9jXHGHE00Arj42eqn',
+      }
+}
 
-# def hello(bot, update):
-#     update.message.reply_text(
-#         'Hello {}'.format(update.message.from_user.first_name))
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
 
-# def start(bot, update):
-#     update.message.reply_text(
-#         'Hi, hi: {}'.format(update.message.from_user.first_name))
+def start(bot, update):
+    update.message.reply_text(
+        'Hi, hi: {}'.format(update.message.from_user.first_name))
 
-# print('wtf')
+print('wtf')
 
-# updater = Updater('778814536:AAGZp7325xBl2fb73PgAI7bQTYfV9Ao44sk',
-#                   request_kwargs=REQUEST_KWARGS)
+updater = Updater('778814536:AAGZp7325xBl2fb73PgAI7bQTYfV9Ao44sk',
+                  request_kwargs=REQUEST_KWARGS)
 
-# print('setup')
+print('setup')
 
 # updater.dispatcher.add_handler(CommandHandler('hello', hello))
 # updater.dispatcher.add_handler(CommandHandler('start', start))
